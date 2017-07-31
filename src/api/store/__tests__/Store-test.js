48aab9a14999f973963976dbc79eb9ffe19dde5c
Pontus Persson <pom@skurt.com>
2017-07-31 08:33:42 +0200

    WIP Store test.

--- src/api/store/__tests__/Store-test.js
+++ src/api/store/__tests__/Store-test.js
@@ -21,12 +21,12 @@ function FakeAdapter() {
 
   this.putStream = sinon.spy((name, stream) => {
     const output = new Writable();
-    let buffer = new Buffer('');
+    const buffers = [];
     sinon.stub(output, '_write').callsFake(data => {
-      buffer = Buffer.concat([buffer, data]);
+      buffers.push(data);
     });
     sinon.stub(output, 'end').callsFake((...args) => {
-      this.files.set(name, buffer);
+      this.files.set(name, Buffer.concat(buffers));
       Reflect.apply(output.end.wrappedMethod, output, args);
       setTimeout(() => output.emit('finish'));
     });
@@ -36,11 +36,13 @@ function FakeAdapter() {
 
 describe('Store', () => {
   const MOCK_ID = 'Aa12xea2';
-  let store;
+  let store, adapter;
 
   beforeEach(() => {
-    store = new Store(new FakeAdapter());
-    sinon.stub(store, 'createId').callsFake(() => 'Aa12xea2');
+    adapter = new FakeAdapter();
+    store = new Store(adapter);
+    sinon.stub(store, 'createId').returns('Aa12xea2');
+    sinon.stub(store, 'createSecret').returns('8q3mu093ny9q0qm8nyntm0r9u0cqynq09mu9q0wuq9-91h-10jf-01901rh1hr1d');
   });
 
   describe('when storing stream', () => {
@@ -58,6 +60,11 @@ describe('Store', () => {
 
     it('returns a Promise', () => {
       expect(storePromise).to.be.a(Promise);
+    });
+
+    it('stores meta data encrypted', () => {
+      expect(adapter.files.get('Aa12xea2.meta').toString('hex'))
+      .to.be('1953b238db9498f23cd5484e584a90316e3f4f534897b0d28ba8188c12c4b579a8d1a70eda1f00186809e0ff7bbe030d81876ca2795d7015b9eb928da30df77c80');
     });
 
     describe('when resolved', () => {
